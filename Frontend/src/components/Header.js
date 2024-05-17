cc0ccfea4e0e77c99cc38c5b9687fbf5595f242c
mayank160903 <97187052+mayank160903@users.noreply.github.com>
2024-05-17 17:21:47 +0530

    backend connected

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   const {setUserInfo, userInfo} = useContext(UserContext);
 
     useEffect(() => {
-        fetch('http://localhost:4000/profile', {
+        fetch('https://myblogsforall.onrender.com/profile', {
             credentials: 'include',
         }).then(response => {
           response.json().then(userInfo => {
@@ -17,7 +17,7 @@ const Header = () => {
     }, []);
 
     function logout() {
-      fetch('http://localhost:4000/logout', {
+      fetch('https://myblogsforall.onrender.com/logout', {
         credentials: 'include',
         method: 'POST',
       })
--- Frontend/src/components/Post.js
+++ Frontend/src/components/Post.js
@@ -10,7 +10,7 @@ const Post = ({_id, title, summary, cover, content, createdAt, author}) => {
     <div className="post">
     <div className="image">
       <Link to={`/post/${_id}`}>
-    <img src={'http://localhost:4000/' + cover} alt="" />
+    <img src={'https://myblogsforall.onrender.com/' + cover} alt="" />
     </Link>
     </div>
     <div className="texts">
